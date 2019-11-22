90e120a4c56d53ec2a1d79e7c6f667020d9fb713
danyalson <danyal.edgerly87@gmail.com>
2019-11-21 19:20:22 -0700

    afternoon project 3 day4

--- practice.js
+++ practice.js
@@ -27,7 +27,9 @@
   Then invoke the callback function, passing in the first element in the array as it's argument.
 */
 
-// Code Here 
+function first(array,callback){
+callback(array[0])
+}
 
 // Do not edit the code below.
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
@@ -47,7 +49,9 @@ first(names, function(firstName){
   Then invoke the callback, passing in the last element in the array as the argument.
 */
 
-//Code Here
+function last(array,callback){
+  callback(array[array.length -1])
+  }
 
 // Do not edit the code below.
 last(names, function(lastName){
@@ -65,7 +69,9 @@ last(names, function(lastName){
   Invoke the callback, passing in the product of the two numbers multiplied as the argument. 
 */
 
-//Code Here
+function multiply(num1, num2, cb){
+cb(num1 * num2)
+}
 
 // Do not edit the code below.
 multiply(4, 3, function(answer){
@@ -84,7 +90,14 @@ multiply(4, 3, function(answer){
   If the name does not exist, invoke the callback with false as the argument.
 */
 
-//Code Here 
+function contains(array,name, cb){
+  for(let i = 0;i<array.length; i++){
+    if(name === array[i]){
+     return cb(true)
+    }
+  }
+  return cb(false)
+}
 
 // Do not edit the code below.
 contains(names, 'Colt', function(result){
